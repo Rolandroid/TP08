b7d86bfe3ac6fd08242116877abf3d9e03bf1242
rolandroid <r.lisarazo@gmail.com>
2023-06-14 19:36:31 -0300

    crea edita y elimina

--- js/formulario.js
+++ js/formulario.js
@@ -1,8 +1,7 @@
 window.onload = async () => {
   const urlParams = new URLSearchParams(window.location.search);
   const valor = urlParams.get('id');
-  console.log(valor)
-  
+
   const title = document.getElementById("title")
   const rating = document.getElementById("rating")
   const awards = document.getElementById("awards")
@@ -12,29 +11,39 @@ window.onload = async () => {
   const editar = document.getElementById("editar")
   const crear = document.getElementById("crear")
   const eliminar = document.getElementById("eliminar")
+  const deleteCheklabel = document.getElementById("deleteCheklabel")
+  const deleteChek = document.getElementById("deleteChek")
   const formCreate = document.getElementById("formCreate")
+
   try {
-    if(valor){
-    const response = await fetch(`http://localhost:3031/api/movies/${valor}`);
-    const data = await response.json();
-    const releaseDate = new Date(data.data.release_date);
+    if (valor) {
+      console.log(valor)
+      crear.style = "display:none"
 
-const day = releaseDate.getDate()
-const month = releaseDate.getMonth() + 1
-const year = releaseDate.getFullYear()
+      const response = await fetch(`http://localhost:3031/api/movies/${valor}`);
+      const data = await response.json();
+      const releaseDate = new Date(data.data.release_date);
 
-const formattedDateFromAPI = `${year}-${String(month).padStart(2, "0")}-${String(day).padStart(2, "0")}`;
-        title.value = data.data.title
-        rating.value =  data.data.rating
-        awards.value =  data.data.awards
-        release_date.value =  formattedDateFromAPI
-        length.value =  data.data.length
-        
-        crear.style = "display:none"
+      const day = releaseDate.getDate()
+      const month = releaseDate.getMonth() + 1
+      const year = releaseDate.getFullYear()
+
+      const formattedDateFromAPI = `${year}-${String(month).padStart(2, "0")}-${String(day).padStart(2, "0")}`;
+      title.value = data.data.title
+      rating.value = data.data.rating
+      awards.value = data.data.awards
+      release_date.value = formattedDateFromAPI
+      length.value = data.data.length
+
+
+
+
+
     }
-    else{
+    else {
       console.log("completa el body")
       editar.style = "display:none"
+      deleteCheklabel.style = "display:none"
       eliminar.style = "display:none"
     }
 
@@ -42,18 +51,18 @@ const formattedDateFromAPI = `${year}-${String(month).padStart(2, "0")}-${String
 
 
 
-    }
-    catch{error=>console.log(error)}  
- 
- 
-    formCreate.addEventListener("submit", async function(event) {
-      event.preventDefault();
-      console.log("se hizo clic");
-    
-      try {
+  }
+  catch { error => console.log(error) }
 
 
-        if(!valor){
+  formCreate.addEventListener("submit", async function (event) {
+    event.preventDefault();
+    console.log("se hizo clic");
+
+    try {
+
+
+      if (!valor) {
         const response = await fetch("http://localhost:3031/api/movies/create/", {
           method: 'POST',
           body: JSON.stringify({
@@ -67,16 +76,18 @@ const formattedDateFromAPI = `${year}-${String(month).padStart(2, "0")}-${String
             'Content-Type': 'application/json'
           }
         });
-    
+
         if (response.ok) {
           console.log("El objeto se creó correctamente");
           // Aquí puedes realizar cualquier acción adicional después de crear el objeto
         } else {
           console.log("Ocurrió un error al crear el objeto");
         }
-      }else{
+      } else if (valor && !deleteChek.checked) {
+
+
         const response = await fetch(`http://localhost:3031/api/movies/update/${valor}`, {
-          method: 'put',
+          method: 'PUT',
           body: JSON.stringify({
             title: title.value,
             rating: rating.value,
@@ -88,25 +99,36 @@ const formattedDateFromAPI = `${year}-${String(month).padStart(2, "0")}-${String
             'Content-Type': 'application/json'
           }
         });
-    
+
+
         if (response.ok) {
           console.log("El objeto se editó correctamente");
+          // Aquí puedes realizar cualquier acción adicional después de crear el objeto
+        } else {
+          console.log("Ocurrió un error al editar el objeto");
+        }
+
+      } else if (valor && deleteChek.checked) {
+        const response = await fetch(`http://localhost:3031/api/movies/delete/${valor}`, {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+        if (response.ok) {
+          console.log("El objeto se Elimino correctamente");
           // Aquí puedes realizar cualquier acción adicional después de crear el objeto
         } else {
           console.log("Ocurrió un error al crear el objeto");
         }
 
+      }else{}
+
+    } catch (error) {
+      console.log(error);
+    }
+  });
+};
 
 
-      }
 
-
-      } catch (error) {
-        console.log(error);
-      }
-    });
-  };
-
- 
-
-  
