553c7d2fc50a1b617a1116430959d5e7b02360fb
rolandroid <r.lisarazo@gmail.com>
2023-06-27 13:30:09 -0300

    correccion

--- js/favoritas.js
+++ js/favoritas.js
@@ -4,35 +4,54 @@ window.onload = () => {
   container.setAttribute("class", "container");
   app.appendChild(container);
 
-  // Aqui debemos agregar nuestro fetch
+  let executeFetch = async () => {
+    try {
+      let response = await fetch("http://localhost:3031/api/movies")
+      let peliculas = await response.json()
+      pintarCards(peliculas)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  executeFetch()
 
 
+  let pintarCards = (peliculas) => {
+    let data = peliculas.data
+    let ids = JSON.parse(sessionStorage.getItem("ids"))
 
-  /** Codigo que debemos usar para mostrar los datos en el frontend
-    let data = peliculas.data;
+    if (!ids) {
+      const msg = document.createElement("h3")
+      msg.textContent = "No tienes películas favoritas actualmente"
+      container.appendChild(msg)
+    } else {
+      data.forEach((movie) => {
+        if (ids && ids.find((element) => element === movie.id)) {
 
-    data.forEach((movie) => {
-      const card = document.createElement("div");
-      card.setAttribute("class", "card");
+          const card = document.createElement("div");
+          card.setAttribute("class", "card");
 
-      const h1 = document.createElement("h1");
-      h1.textContent = movie.title;
+          const h1 = document.createElement("h1");
+          h1.textContent = movie.title;
 
-      const p = document.createElement("p");
-      p.textContent = `Rating: ${movie.rating}`;
+          const p = document.createElement("p");
+          p.textContent = `Rating: ${movie.rating}`;
 
-      const duracion = document.createElement("p");
-      duracion.textContent = `Duración: ${movie.length}`;
+          const duracion = document.createElement("p");
+          duracion.textContent = `Duración: ${movie.length}`;
 
-      container.appendChild(card);
-      card.appendChild(h1);
-      card.appendChild(p);
-      if (movie.genre !== null) {
-        const genero = document.createElement("p");
-        genero.textContent = `Genero: ${movie.genre.name}`;
-        card.appendChild(genero);
-      }
-      card.appendChild(duracion);
-    });
-  */
+          container.appendChild(card);
+          card.appendChild(h1);
+          card.appendChild(p);
+          if (movie.genre !== null) {
+            const genero = document.createElement("p");
+            genero.textContent = `Genero: ${movie.genre.name}`;
+            card.appendChild(genero);
+          }
+          card.appendChild(duracion);
+
+        }
+      })
+    }
+  }
 };
--- js/main.js
+++ js/main.js
@@ -1,56 +1,89 @@
 window.onload = () => {
   const app = document.getElementById("root");
+  const favoritas = document.getElementById("favoritas");
   const container = document.createElement("div");
   container.setAttribute("class", "container");
   app.appendChild(container);
-  
+
+  favoritas.style = "display:none";
+  localStorage.setItem("ids", localStorage.getItem("ids") || "");
+
+  if (localStorage.getItem("ids").length > 0) {
+    favoritas.style = "display:inline";
+  }
+
+  let favoriteIds = localStorage.getItem("ids") ?? [];
+
   // Aquí debemos agregar nuestro fetch
   const obtenerPeliculas = async () => {
     try {
-      const response = await fetch('http://localhost:3031/api/movies/');
+      const response = await fetch("http://localhost:3031/api/movies/");
       const data = await response.json();
-      
-     
 
-    // Código que debemos usar para mostrar los datos en el frontend
-    console.log(data);
-    data.data.forEach((movie) => {
-      const card = document.createElement("div");
-      card.setAttribute("class", "card");
+      // Código que debemos usar para mostrar los datos en el frontend
+      data.data.forEach((movie) => {
+        const card = document.createElement("div");
+        card.setAttribute("class", "card");
 
-      const h1 = document.createElement("h1");
-      h1.textContent = movie.title;
+        const h1 = document.createElement("h1");
+        h1.textContent = movie.title;
 
-      const p = document.createElement("p");
-      p.textContent = `Rating: ${movie.rating}`;
+        const p = document.createElement("p");
+        p.textContent = `Rating: ${movie.rating}`;
 
-      const duracion = document.createElement("p");
-      duracion.textContent = `Duración: ${movie.length}`;
-      
-      const edicion = document.createElement("a");
-      edicion.href = `formulario.html?id=${movie.id}`;
-      edicion.textContent = `Editar`
-      edicion.className = `botonAgregar`
-      
+        const duracion = document.createElement("p");
+        duracion.textContent = `Duración: ${movie.length}`;
 
+        const edicion = document.createElement("a");
+        edicion.href = `formulario.html?id=${movie.id}`;
+        edicion.textContent = `Editar`;
+        edicion.className = `botonAgregar`;
 
-      container.appendChild(card);
-      card.appendChild(h1);
-      card.appendChild(p);
-      if (movie.genre !== null) {
-        const genero = document.createElement("p");
-        genero.textContent = `Género: ${movie.genre.name}`;
-        card.appendChild(genero);
-      }
-      card.appendChild(duracion);
-      card.appendChild(edicion);
+        container.appendChild(card);
+        card.appendChild(h1);
+        card.appendChild(p);
+        if (movie.genre !== null) {
+          const genero = document.createElement("p");
+          genero.textContent = `Género: ${movie.genre.name}`;
+          card.appendChild(genero);
+        }
+        card.appendChild(duracion);
+        card.appendChild(edicion);
+
+        const addButton = document.createElement("button");
+        addButton.setAttribute("class", "botonAgregar");
+        addButton.textContent = "Agregar a favoritos";
+        card.appendChild(addButton);
+
+        if (favoriteIds.includes(movie.id.toString())) {
+          addButton.textContent = "Remover de Favoritos";
+        }
+
+        addButton.addEventListener("click", () => {
+          toggleFavorite(movie.id);
+          if (favoriteIds.includes(movie.id.toString())) {
+            addButton.textContent = "Agregar a favoritos";
+          } else {
+            addButton.textContent = "Remover de favoritos";
+          }
+        });
+      });
+    } catch (error) {
+      console.log("Error al obtener las películas:", error);
     }
-    );
-  } catch (error) {
-    console.log('Error al obtener las películas:', error);
+  };
+
+  function toggleFavorite(movieId) {
+    const movieIdString = movieId.toString();
+    const index = favoriteIds.indexOf(movieIdString);
+    if (index > -1) {
+      favoriteIds.splice(index, 1);
+    } else {
+      favoriteIds.push(movieIdString);
+    }
+
+    localStorage.setItem("ids", JSON.stringify(favoriteIds));
   }
+
+  obtenerPeliculas();
 };
-
-obtenerPeliculas();
-
-}
